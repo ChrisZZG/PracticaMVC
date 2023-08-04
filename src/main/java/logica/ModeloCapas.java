c43b43ca8b9473203c5090b07032a905b29180be
ChrisZZG <cristiangonzz@gmail.com>
2023-08-04 17:27:05 -0300

    Primera Carga Practica

---
+++ src/main/java/logica/ModeloCapas.java
@@ -0,0 +1,17 @@
+
+
+package logica;
+
+import igu.Pantalla;
+
+
+public class ModeloCapas {
+
+    public static void main(String[] args) {
+        
+        //Hacer que la pantalla sea ejecutable
+        Pantalla panta = new Pantalla();
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+    }
+}
